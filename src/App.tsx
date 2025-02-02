a58eed04da9916395b6863da6e11d9e6543684c5
stnsc <stanescuvladutgeorge@gmail.com>
2025-02-02 23:07:04 +0200

    Added Download Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar"
 import Personal from "./components/Personal"
 import Title from "./components/Title"
 import Work from "./components/Work"
+import Download from "./components/Download"
 
 function App() {
     const [activePage, setActivePage] = useState("personal")
@@ -14,6 +15,7 @@ function App() {
             <Title />
             <Navbar setActivePage={setActivePage} />
             {activePage === "personal" ? <Personal /> : <Work />}
+            <Download />
         </>
     )
 }
---
+++ src/components/Download.tsx
@@ -0,0 +1,22 @@
+import { DocumentArrowDownIcon } from "@heroicons/react/24/solid"
+
+function Download() {
+    return (
+        <>
+            <div className="download-div flex justify-center">
+                <button type="button" className="button-download ">
+                    <a
+                        target="_blank"
+                        href="../../public/CV - Stanescu Vladut.pdf"
+                        className="flex flex-row"
+                    >
+                        <DocumentArrowDownIcon className="size-6" />
+                        Download the resume in a simple PDF format
+                    </a>
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default Download
